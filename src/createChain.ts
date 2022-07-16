4e0c007fff17236e4d995b101c3eedf4b571e5ec
Kmyrrpen <shanenescarimoro@gmail.com>
2022-07-16 17:02:10 +0800

    removed createDispatch in favor of chaining chains together, removed optional next

--- src/createChain.ts
+++ src/createChain.ts
@@ -1,37 +1,29 @@
 import { Action } from "./createAction";
-import { Dispatch, emptyNext } from "./createDispatch";
 
+export const end = (action: Action) => action;
+export type Dispatch = (action: Action<any>) => void;
 export type Next = (action: Action<any>) => any;
 export type Flow = (action: Action<any>, dispatch: Dispatch, next: Next) => any;
 
 export function createChain(...flows: Flow[]) {
-  if (flows.length === 0) {
-    throw Error("array must not be empty");
-  }
-
   let memoizedRootFlow: Next;
-
-  function createRootFlow(dispatch: Dispatch) {
-    let rootFlow: Next = emptyNext;
-    let tempArr: Next[] = [emptyNext];
-
+  function createRootFlow(dispatch: Dispatch, next: Next = end) {
+    let rootFlow: Next = next;
     for (let i = flows.length - 1; i >= 0; i--) {
-      rootFlow = (action) => flows[i](action, dispatch, tempArr[flows.length - 1 - i]);
-      tempArr.push(rootFlow);
+      const temp = rootFlow;
+      rootFlow = (action) => flows[i](action, dispatch, temp);
     }
-
-    return rootFlow as Next;
+    return rootFlow;
   }
 
-  return function chainDispatch(action: Action<any>, dispatch?: Dispatch) {
-    // if dispatch isn't given, reference self instead.
-    dispatch = dispatch || chainDispatch;
-
-    // create rootFlow if haven't yet.
+  return function chainDispatch(
+    action: Action<any>,
+    dispatch?: Dispatch,
+    next?: Next
+  ) {
     if (!memoizedRootFlow) {
-      memoizedRootFlow = createRootFlow(dispatch);
+      memoizedRootFlow = createRootFlow(dispatch || chainDispatch, next || end);
     }
-
     return memoizedRootFlow(action);
   };
 }
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,5 @@
 import { createAction } from "./createAction";
 import { Flow, createChain } from "./createChain";
-import { createDispatch } from "./createDispatch";
 
 const type = "string";
 const type2 = "number";
@@ -27,22 +26,16 @@ describe("createAction", () => {
 });
 
 describe("createChain", () => {
-  let store: string[];
-
-  beforeEach(() => {
-    store = [];
-  });
-
   const action1 = stringActionCreator("string");
   const action2 = numberActionCreator(0);
 
-  const flow1: Flow = (action, _, next) => next(action);
-  const flow2: Flow = (action, _, next) => next(action);
-  const setter: Flow = (action, _) => {
-    store.push(action.payload);
-  };
-
   test("dispatching actions", () => {
+    let store: string[] = [];
+    const flow1: Flow = (action, _, next) => next(action);
+    const flow2: Flow = (action, _, next) => next(action);
+    const setter: Flow = (action, _) => {
+      store.push(action.payload);
+    };
     const chainDispatch = createChain(flow1, flow2, setter);
     chainDispatch(action1);
     chainDispatch(action2);
@@ -50,25 +43,24 @@ describe("createChain", () => {
     expect(store[1]).toBe(action2.payload);
   });
 
-  test("dispatch function is correct", () => {
+  test("dispatch function is passed correctly", () => {
     const flow1: Flow = (action, dispatch, next) =>
-      next({ type: "dispatch", payload: dispatch });
+    next({ type: "dispatch", payload: dispatch });
     const flow2: Flow = (action, dispatch, next) => ({
       from1: action.payload,
       from2: dispatch,
     });
     const chainDispatch = createChain(flow1, flow2);
-    //@ts-ignore
     expect(
       chainDispatch === chainDispatch({ type: "", payload: "" }).from1
-    ).toBe(true);
-    //@ts-ignore
+      ).toBe(true);
     expect(
       chainDispatch === chainDispatch({ type: "", payload: "" }).from2
-    ).toBe(true);
+      ).toBe(true);
   });
-
-  test("order of flows", () => {
+  
+  test("order of flows is correct", () => {
+    let store: string[] = [];
     let order = "";
 
     const flow1: Flow = (action, _, next) => {
@@ -105,100 +97,31 @@ describe("createChain", () => {
     expect(order).toBe("1.2.3.1.2.3.4.4.");
     expect(store).toContain("reached flow 4");
   });
-});
 
-describe("createDispatch", () => {
-  let order: string;
-  const action1 = stringActionCreator("");
+  test("empty args", () => {
+    const dispatch = createChain();
+    dispatch(action1); // no err
+  })
 
-  beforeEach(() => {
-    order = "";
-  });
-
-  test("correct order of flows", () => {
-    const flow1: Flow = (action, dispatch) => {
-      order += "1.";
-    };
-  
-    const flow2: Flow = (action, dispatch) => {
-      order += "2.";
-    };
-
-    const dipsatch = createDispatch(flow1, flow2);
-    dipsatch(action1);
-    expect(order).toBe("1.2.");
-  });
-
-  test("dispatch ignores next calls", () => {
-    const flow1: Flow = (action, dispatch) => {
-      order += "1.";
-    };
-  
-    const flow2: Flow = (action, dispatch) => {
-      order += "2.";
-    };
-
-    const flowThatCallsNext: Flow = (action, dispatch, next) => {
-      next(action);
-      order += "next.";
-    };
-    const dispatch = createDispatch(flow1, flowThatCallsNext, flow2);
-    dispatch(action1);
-    expect(order).toBe("1.next.2.");
-  });
-
-  test("dispatch handles chains", () => {
-    const action1 = stringActionCreator("string");
-
+  test("chains inside chains inside chains", () => {
+    let order = "";
     const flow1: Flow = (action, dispatch, next) => {
       order += "1.";
       next(action);
     };
-
     const flow2: Flow = (action, dispatch, next) => {
       order += "2.";
       next(action);
-    };
-
-    const chain1 = createChain(flow1, flow2);
-    const chain2 = createChain(flow2, flow1);
-
-    const dispatch = createDispatch(flow1, flow2, chain1, chain2);
-    dispatch(action1);
-    expect(order).toBe("1.2.1.2.2.1.");
-  });
-
-  test("dispatch is properly passed to chains", () => {
-    const action1 = stringActionCreator("string");
-    const action2 = numberActionCreator(0);
-
-    const flow1: Flow = (action, _, next) => {
-      order += "1.";
-      next(action);
-    };
-
-    const flow2: Flow = (action, _, next) => {
-      order += "2.";
-      next(action);
-    };
-
+    }
     const flow3: Flow = (action, dispatch, next) => {
       order += "3.";
-      if(stringActionCreator.match(action)) {
-        dispatch(action2);
-      }
-
-      next(action);
-    }
-    
-    const flow4: Flow = (action, _, next) => {
-      order += "4.";
       next(action);
     }
 
-    const chain = createChain(flow1, flow2, flow3, flow4);
-    const dispatch = createDispatch(flow4, flow2, chain, flow4);
-    dispatch(action1);
-    expect(order).toBe("4.2.1.2.3.4.2.1.2.3.4.4.4.4.");
+    const chain1 = createChain(flow1, flow2);
+    const chain2 = createChain(flow2, flow1, chain1);
+    const masterChain = createChain(chain2, flow3);
+    masterChain(action1);
+    expect(order).toBe("2.1.1.2.3.");
   })
 });
